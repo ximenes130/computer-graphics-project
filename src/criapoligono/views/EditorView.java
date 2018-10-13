095854d70547b2e61f176cdec7e109fdb4749e2a
Luiz Ximenes <ximenes130@gmail.com>
2018-10-13 16:01:23 -0300

    Adicionando exemplo, como desenhar um triangulo

--- src/criapoligono/views/EditorView.java
+++ src/criapoligono/views/EditorView.java
@@ -1,5 +1,6 @@
 package criapoligono.views;
 
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
@@ -18,47 +19,59 @@ public class EditorView implements GLEventListener {
     private GLCanvas glcanvas;
             
     public EditorView() {
-        //getting the capabilities object of GL2 profile
+        // Definido GL2 como perfil OpenGL
         final GLProfile profile = GLProfile.get(GLProfile.GL2);
         GLCapabilities capabilities = new GLCapabilities(profile);
 
-        // The canvas
+        // Criando o canvas
         glcanvas = new GLCanvas(capabilities);
-        JFrame b = new JFrame("FrameDemo");
-        // glcanvas.addGLEventListener(b);        
+        glcanvas.addGLEventListener(this); 
         glcanvas.setSize(400, 400);
 
-        //creating frame
+        // Criando frame (Janela)
         windows = new JFrame (" Basic Frame");
 
-        //adding canvas to frame
-        windows.add(glcanvas);
+        // Adicionando o canvas no frame
+        windows.getContentPane().add(glcanvas);
+        
+        // Configurando o frame
         windows.setSize( 800, 600 );
+        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         windows.setVisible(true);
         System.out.println("Janela exibida!");
     }
     
     @Override
-    public void display(GLAutoDrawable arg0) {
-      // method body
+    public void display(GLAutoDrawable drawable) {
+        final GL2 gl = drawable.getGL().getGL2();
+
+        // Desenhando um tiangulo
+        gl.glBegin(GL2.GL_TRIANGLES);
+        
+        // Definindo cor do desenho
+        gl.glColor3f( 0f,1f,0f );
+
+        // Definindo vertices do triangulo
+        gl.glVertex3f(0.5f,0.7f,0.0f);       // Topo
+        gl.glVertex3f(-0.2f,-0.50f,0.0f);    // Inferior esquerdo
+        gl.glVertex3f(0.5f,-0.5f,0.0f);      // Inferior direito
+
+        // Finalizando desenho da forma
+        gl.glEnd();
     }
 
     @Override
-    public void dispose(GLAutoDrawable arg0) {
+    public void dispose(GLAutoDrawable drawable) {
       //method body
     }
 
     @Override
-    public void init(GLAutoDrawable arg0) {
+    public void init(GLAutoDrawable drawable) {
       // method body
     }
 
     @Override
-    public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
+    public void reshape(GLAutoDrawable drawable, int arg1, int arg2, int arg3, int arg4) {
       // method body
     }
-    
-    public static void main(String[] args) {
-
-   }
 }
